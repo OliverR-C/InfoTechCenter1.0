cd21ede66cbc0acb31bc681082f878013a45a4c7
Rose-Clementson <oliver.roseclementson@gmail.com>
2024-10-11 07:46:14 -0400

    Added a timeToSleep variable to adjust my sleep time

--- CastleBravo.py
+++ CastleBravo.py
@@ -1,1 +1,33 @@
+
+
+
 print("\nWelcome to InforTechCenter V1.0")
+
+
+
+
+
+CYAN =
+
+
+
+print(CYAN + "\n Welcome to InfoTechCenter V1.0\n" + RESET)
+
+timeToSleep = 2 #varaible to set the time library to 2 seconds when called
+time.sleep(timeToSleep)  #Calling the time to sleep library with the variable timeToSleeps value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
