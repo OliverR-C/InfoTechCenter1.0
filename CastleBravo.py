b09b90bc6827c6801306526dc0c8d829c348b9af
Rose-Clementson <oliver.roseclementson@gmail.com>
2024-10-29 07:47:04 -0400

    Added ELIF's

--- CastleBravo.py
+++ CastleBravo.py
@@ -1,33 +1,44 @@
+import time
+import sys
 
+def log(message, level="INFO"):
+    print(f"[{level}] {message}")
 
+LOG_LEVELS = {
+    "INFO": "INFO",
+    "WARNING": "WARNING",
+    "ERROR": "ERROR",
+}
 
-print("\nWelcome to InforTechCenter V1.0")
+def log_message(level, message):
+    level_str = LOG_LEVELS.get(level, "INFO")
+    log(message, level=level_str)
 
+for i in range(5):
+    if i == 0:
+        log_message("INFO", "Process started")
+    elif i == 3:
+        log_message("WARNING", "CPU usage is high")
+    elif i == 4:
+        log_message("ERROR", "Process failed")
+    else:
+        log_message("INFO", f"Running iteration {i}")
 
+x = 0
+ellipsis = 0
 
+timetosleep = 4
+time.sleep(timetosleep)
 
+while x != 20:
+    x += 1
+    message = "InfoTech Center System Booting" + "." * ellipsis
+    ellipsis += 1
+    sys.stdout.write("\r" + message)
+    time.sleep(0.2)
 
-CYAN =
+    if ellipsis == 4:
+        ellipsis = 0
 
-
-
-print(CYAN + "\n Welcome to InfoTechCenter V1.0\n" + RESET)
-
-timeToSleep = 2 #varaible to set the time library to 2 seconds when called
-time.sleep(timeToSleep)  #Calling the time to sleep library with the variable timeToSleeps value
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if x == 20:
+    print("\nOperating System Booted Up - Retina Scanned - Access Granted")
